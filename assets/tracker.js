eaa9944a245951d92563d45b5ffa73d4f6f353ef
Kirill Kayer <kirill@moveparallel.com>
2026-06-13 09:44:50 -0700

    Fmt

--- assets/tracker.js
+++ assets/tracker.js
@@ -1,13 +1,14 @@
 (function () {
-  'use strict';
+  "use strict";
 
-  if (navigator.doNotTrack === '1' || window.doNotTrack === '1') return;
+  if (navigator.doNotTrack === "1" || window.doNotTrack === "1") return;
 
-  var script = document.currentScript || document.querySelector('script[data-site]');
-  if (!script || script.hasAttribute('data-exclude')) return;
+  var script =
+    document.currentScript || document.querySelector("script[data-site]");
+  if (!script || script.hasAttribute("data-exclude")) return;
 
-  var endpoint = script.getAttribute('data-api') || '/api/v1/event';
-  var siteKey = script.getAttribute('data-site');
+  var endpoint = script.getAttribute("data-api") || "/api/v1/event";
+  var siteKey = script.getAttribute("data-site");
   if (!siteKey) return;
 
   function send(name, props) {
@@ -23,22 +24,38 @@
       t: Date.now(),
     });
     if (navigator.sendBeacon) {
-      navigator.sendBeacon(endpoint, new Blob([payload], { type: 'application/json' }));
+      navigator.sendBeacon(
+        endpoint,
+        new Blob([payload], { type: "application/json" }),
+      );
     } else {
-      fetch(endpoint, { method: 'POST', body: payload, keepalive: true, headers: { 'Content-Type': 'application/json' } });
+      fetch(endpoint, {
+        method: "POST",
+        body: payload,
+        keepalive: true,
+        headers: { "Content-Type": "application/json" },
+      });
     }
   }
 
-  send('pageview');
+  send("pageview");
 
   var _push = history.pushState;
   var _replace = history.replaceState;
-  function onNav() { send('pageview'); }
-  history.pushState = function () { _push.apply(this, arguments); onNav(); };
-  history.replaceState = function () { _replace.apply(this, arguments); onNav(); };
-  window.addEventListener('popstate', onNav);
+  function onNav() {
+    send("pageview");
+  }
+  history.pushState = function () {
+    _push.apply(this, arguments);
+    onNav();
+  };
+  history.replaceState = function () {
+    _replace.apply(this, arguments);
+    onNav();
+  };
+  window.addEventListener("popstate", onNav);
 
   window.stomatopod = function (type, name, props) {
-    if (type === 'event') send(name, props);
+    if (type === "event") send(name, props);
   };
 })();
